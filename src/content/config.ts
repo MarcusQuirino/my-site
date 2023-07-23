8759cd1258fd337c95b39832c2c47c773e9054a2
MarcusQuirino <marcusfilipus@gmail.com>
2023-07-23 13:22:37 -0300

    idk :3

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,22 @@
 import { defineCollection, z } from 'astro:content';
 
-const values = ["typescript", "javascript", "react", "next", "astro", "node", "rust","python", "docker", "aws", "html", "css", "dev", "life", "misc"] as const;
+const tagValues = ["typescript", "javascript", "react", "next", "astro", "node", "rust","python", "docker", "aws", "html", "css", "dev", "life", "misc"] as const;
+const categoryTypes = ["hardware", "software"] as const;
 
-const tags = z.enum(values);
+const tags = z.enum(tagValues);
+const category = z.enum(categoryTypes);
 
 export type Tags = z.infer<typeof tags>
+export type Category = z.infer<typeof category>
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
 	schema: z.object({
-		author: z.string().default('Marcus FIlipus'),
+		author: z.string().default('Marcus Filipus'),
 		title: z.string(),
 		description: z.string(),
 		highligth: z.boolean().default(false),
 		tags: z.array(tags).nonempty({
-			message: "Can't be empty!: needs tags for blog post",
+			message: "Can't be empty!: Needs tags for blog post",
 		  }),
 		pubDate: z
 			.string()
@@ -28,11 +30,18 @@ const blog = defineCollection({
 	}),
 });
 
-const assets = defineCollection({
+const reviews = defineCollection({
 	schema: z.object({
-		title: z.string(),
-		text: z.string(),
+		name: z.string(),
+		image: z.string(),
+		afiliateLink: z.string(),
+		using: z.boolean().default(true),
+		pubDate: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+		category: category,
 	}),
 });
 
-export const collections = { blog, assets };
+export const collections = { blog, reviews };
