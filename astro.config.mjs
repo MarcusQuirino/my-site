a1a63f1f3e59d6703b300422b4044df20544f3c8
MarcusQuirino <marcusfilipus@gmail.com>
2023-10-08 14:02:30 -0300

    upgrade do astro v3 :3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/static';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
   output: 'static',
-  adapter: vercel({
-    analytics: true,
-    imageService: true,
-    imagesConfig: {
-      sizes: [320, 640, 1200],
-      formats: ['image/avif']
-    }
-  }),
-  experimental: {
-    viewTransitions: true,
-    assets: true
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop',
+    },
   },
-});
+  adapter: vercel(),
+})
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,48 +1,66 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from 'astro:content'
 
-const tagValues = ["typescript", "javascript", "react", "next", "astro", "node", "rust","python", "docker", "aws", "html", "css", "dev", "life", "misc"] as const;
-const categoryTypes = ["hardware", "software"] as const;
+const tagValues = [
+  'typescript',
+  'javascript',
+  'react',
+  'next',
+  'astro',
+  'node',
+  'rust',
+  'python',
+  'docker',
+  'aws',
+  'html',
+  'css',
+  'dev',
+  'life',
+  'misc',
+] as const
+const categoryTypes = ['hardware', 'software'] as const
 
-const tags = z.enum(tagValues);
-const category = z.enum(categoryTypes);
+const tags = z.enum(tagValues)
+const category = z.enum(categoryTypes)
 
 export type Tags = z.infer<typeof tags>
 export type Category = z.infer<typeof category>
 
 const blog = defineCollection({
-	schema: ({ image }) => z.object({
-		author: z.string().default('Marcus Filipus'),
-		title: z.string(),
-		description: z.string(),
-		highligth: z.boolean().default(false),
-		tags: z.array(tags).nonempty({
-			message: "Can't be empty!: Needs tags for blog post",
-		  }),
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: image()
-	}),
-});
+  schema: ({ image }) =>
+    z.object({
+      author: z.string().default('Marcus Filipus'),
+      title: z.string(),
+      description: z.string(),
+      highligth: z.boolean().default(false),
+      tags: z.array(tags).nonempty({
+        message: "Can't be empty!: Needs tags for blog post",
+      }),
+      pubDate: z
+        .string()
+        .or(z.date())
+        .transform(val => new Date(val)),
+      updatedDate: z
+        .string()
+        .optional()
+        .transform(str => (str ? new Date(str) : undefined)),
+      heroImage: image(),
+    }),
+})
 
 const reviews = defineCollection({
-	schema: ({ image }) => z.object({
-		id: z.number(),
-		name: z.string(),
-		image: image(),
-		afiliateLink: z.string().optional(),
-		using: z.boolean().default(true),
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		category: category,
-	}),
-});
+  schema: ({ image }) =>
+    z.object({
+      id: z.number(),
+      name: z.string(),
+      image: image(),
+      afiliateLink: z.string().optional(),
+      using: z.boolean().default(true),
+      pubDate: z
+        .string()
+        .or(z.date())
+        .transform(val => new Date(val)),
+      category: category,
+    }),
+})
 
-export const collections = { blog, reviews };
+export const collections = { blog, reviews }
